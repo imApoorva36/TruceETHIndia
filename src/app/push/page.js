57fcf05d8d6acf89196e22812ef2fdb0c41338b2
Testing wala User <apoorva3569@gmail.com>
2023-12-09 01:07:07 +0530

    Code to send sample notification

--- src/app/push/page.js
+++ src/app/push/page.js
@@ -2,30 +2,34 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import s from './push.module.css';
-import { useCookies } from 'react-cookie';
 import { PushAPI, CONSTANTS } from "@pushprotocol/restapi";
 import { ethers } from "ethers";
 
 const ScoreboardPage = () => {
-  useAuth();
   const router = useRouter();// Creating a random signer from a wallet, ideally this is the wallet you will connect
-  const signer = ethers.Wallet.createRandom();
   useEffect(() => {
     async function fetchpush() {
       try {
-        const userAlice = await PushAPI.initialize(signer, { env: CONSTANTS.ENV.STAGING });
-        const response = await userAlice.channel.send(["*"], {
-            notification: {
-              title: "You awesome notification",
-              body: "from your amazing protocol",
-            },
-        });
+        
+        if (window.ethereum) {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const userAlice = await PushAPI.initialize(signer, { env: CONSTANTS.ENV.STAGING });
+            const response = await userAlice.channel.send(["*"], {
+                notification: {
+                title: "hahahaha this is my what do u think uh, ahahahahhaahaha awesome notification",
+                body: "from your amazing protocol",
+                },
+            });
 
-        if (response.ok) {
-          const data = await response.json();
-          
+            if (response.ok) {
+            // const data = await response.json();
+            
+            } else {
+            console.error('Error in data of notification :', response.status);
+            }
         } else {
-          console.error('Error in data of notification :', response.status);
+            console.error("MetaMask is not installed");
         }
       } catch (error) {
         console.error('Network error:', error);
