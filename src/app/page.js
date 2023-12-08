e4045b5550f3d0e846948144280bc4a43975fa6d
Hariharan <jhariharan2004@gmail.com>
2023-12-09 01:28:24 +0530

    Added home page and useWallet hook

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,34 @@
+"use client"
+
+import useWallet from "./_helpers/wallet"
 import s from "./home.module.css"
 
 export default function Home () {
+	let [ wallet, login, logout ] = useWallet()
+
 	return (
 		<main className = {s.home}>
-			
+			<div className = {s.content}>
+				<h1>OrgETH</h1>
+				<h2>Transparent spendings for organisations.</h2>
+				<div className={s.buttons}>
+					<div className={s.buttons}>
+						{
+							wallet == null
+							?
+							<>
+								<div className="button bright" onClick={login}>Register</div>
+								<div className="button minimal">Login</div>
+							</>
+							:
+							<>
+								<div className="button bright">Dashboard</div>
+								<div className="button minimal" onClick={logout}>Logout</div>
+							</>
+						}
+					</div>
+				</div>
+			</div>
 		</main>
 	)
 }
