95b3c3bbcceacdfb84f9809ee379ca4734b06311
Testing wala User <apoorva3569@gmail.com>
2023-12-10 02:49:13 +0530

    Modified opting in in notification route option

--- src/app/dashboard/organisations/[id]/notifications/page.js
+++ src/app/dashboard/organisations/[id]/notifications/page.js
@@ -1,6 +1,8 @@
 "use client"
 
 import { useEffect, useState } from "react"
+import { PushAPI, CONSTANTS } from "@pushprotocol/restapi";
+import { ethers } from "ethers";
 import s from "./notifications.module.css"
 import { getOrganizationDetails } from "@/app/_helpers/organisation"
 
@@ -9,10 +11,27 @@ export default function Notifications ({ params }) {
 
     useEffect(() => {
         async function getData () {
-            let res = await getOrganizationDetails(params.getOrganizationDetails
-                )
-        }
-        
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const userAlice = await PushAPI.initialize(signer, { env: CONSTANTS.ENV.STAGING });
+            const aliceSubscriptions = await userAlice.notification.subscriptions();
+            console.log(aliceSubscriptions);
+
+            const channelAddresses = aliceSubscriptions.map(subscription => subscription.channel);
+
+            // Current wallet address (replace this with your actual wallet address)
+            const currentWalletAddress = "0xf00b9A1a33703159D76255900535d9bccA431F5B";
+
+            const isMatch = channelAddresses.some(address => address.toLowerCase() === currentWalletAddress.toLowerCase());
+
+            if (isMatch) {
+            console.log("Match found! The wallet address is subscribed to one of the channels.");
+            } else {
+            console.log("No match found. The wallet address is not subscribed to any of the channels.");
+            }
+
+        }  
+        getData();   
     }, [])
     return <></>
 }
